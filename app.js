a74fb8960377cb7bdd00009c1c6ddf9f7c8af00e
Shaun Dawson <iamsdawson@gmail.com>
2021-03-11 20:43:26 -0500

    implement loading image, improve user experience

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // Listen for Submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -24,14 +32,25 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2)
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
-  e.preventDefault();
 }
 
 // Show error 
 function showError(error) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide loader
+  document.getElementById('loading').style.display = 'none';
+
   // Create a div
   const errorDiv = document.createElement('div');
 
@@ -53,7 +72,7 @@ function showError(error) {
 }
 
 // Clear error 
-function clearError(){
+function clearError() {
   document.querySelector('.alert').remove();
 }
 
